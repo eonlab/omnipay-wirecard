b3b458672271d68e796697911a04fadefc00c6a9
eonlab <luisfsd@runbox.com>
2016-03-03 18:15:22 +0000

    Update PaymentTransactionBuilder.php

--- src/Message/TransactionBuilder/PaymentTransactionBuilder.php
+++ src/Message/TransactionBuilder/PaymentTransactionBuilder.php
@@ -3,9 +3,11 @@
 namespace Omnipay\Wirecard\Message\TransactionBuilder;
 
 use Omnipay\Common\CreditCard;
+use Wirecard\Element\Amount;
 use Wirecard\Element\CreditCardData;
 use Wirecard\Element\Secure;
 use Wirecard\Element\Transaction;
+use Wirecard\Element\RecurringTransaction;
 
 class PaymentTransactionBuilder extends EnrollmentTransactionBuilder
 {
@@ -16,17 +18,19 @@ class PaymentTransactionBuilder extends EnrollmentTransactionBuilder
             $transaction->id = $this->request->getTransactionId();
             $transaction->guWid = $this->request->getTransactionReference();
             $transaction->creditCardData = new CreditCardData();
+            $transaction->amount = new Amount($this->request->getAmount());
+            $transaction->currency = $this->request->getCurrency();
+            $transaction->recurringTransaction = new RecurringTransaction('Repeated');
 
             if ($this->request->getToken()) {
                 $transaction->secure = Secure::createResponse($this->request->getToken());
             }
         } else {
             $transaction = parent::build();
+            /** @var CreditCard $creditCard */
+            $creditCard = $this->request->getCard();
+            $transaction->creditCardData->secureCode = $creditCard->getCvv();
         }
-
-        /** @var CreditCard $creditCard */
-        $creditCard = $this->request->getCard();
-        $transaction->creditCardData->secureCode = $creditCard->getCvv();
 
         return $transaction;
     }
